71b85a95c242321af49ea1a33c94248dfef3ff6c
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2023-11-07 15:56:29 -0300

    Update class Account.java with Card.java

--- src/main/java/me/dio/domain/model/Account.java
+++ src/main/java/me/dio/domain/model/Account.java
@@ -15,10 +15,10 @@ public class Account {
     private String number;
     private String agency;
 
-    @Column( scale = 13, precision = 2)
+    @Column( scale = 2, precision = 13)
     private BigDecimal balance;
 
-    @Column(name = "additional_limit", scale = 13, precision = 2)
+    @Column(name = "additional_limit", scale = 2, precision = 13)
     private BigDecimal limit;
 
     public Long getId() {
